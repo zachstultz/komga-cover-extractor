5dc8342906a48cf5a20940b3f650de673520d4e0
Zach Stultz <hcazs4@live.com>
2021-05-03 10:34:02 -0500

    added cover_detection_strings array started support .cbr files

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import os
 import shutil
+import time
 import zipfile
 import zlib
+from html.parser import HTMLParser
 
 # ************************************
 # Created by: Zach Stultz            *
-# Date: 04-28-21-04-29-21            *
 # Git: https://github.com/zachstultz *
 # ***************************************************
 # A manga/light novel processor script for use with *
 # the manga/light novel reader gotson/komga.        *
-# ***************************************************
+# ************************************************************************************************************************
+# REMINDER : Add logic for a .rar file, aka .cbr file, also add logic for parsing cover.xhtml for exact cover image name *
+# ************************************************************************************************************************
 
 # [ADD IN THE PATHS YOU WANT SCANNED]
-paths = ["Z:\\manga", "Y:\\torrents", "Z:\\novels"]
+paths = ["Y:\\torrents", "Z:\\manga", "Z:\\novels"]
 # [ADD IN THE PATHS YOU WANT SCANNED]
 
 # List of image types used throughout the program
 image_types = ["jpg", "jpeg", "png", "tbn"]
 
+# List of cover strings used for detection
+cover_detection_strings = ["Cover", "cover", "p000", "000 ", "-000", "000a", "_000.", " 001.", "index-1_1"]
+
+volume_detection_strings = ["v01", "Volume 1", "volume 1", "volume 01", "Volume 01", "volume one", "Volume one",
+                            "Volume One", "LN 01"]
+
 # Stat-related variables
 file_count = 0
 cbz_count = 0
 epub_count = 0
+cbr_count = 0
 image_count = 0
 cbz_internal_covers_found = 0
 poster_found = 0
@@ -77,6 +87,7 @@ def check_internal_zip_for_cover(file, full_path, root):
     cover_found = 0
     poster_found = 0
     try:
+        # Add logic for a .rar file, aka .cbr file
         if zipfile.is_zipfile(full_path):
             zip_file = zipfile.ZipFile(full_path)
             print("\n" + "Zip found\n" + "Entering zip: " + file)
@@ -89,20 +100,24 @@ def check_internal_zip_for_cover(file, full_path, root):
                 head_tail = os.path.split(item)
                 file_path = head_tail[0]
                 image_file = head_tail[1]
-                if ((item.__contains__('cover') | item.__contains__('Cover') | item.__contains__(
-                        "p000") | item.__contains__('000 ') | item.__contains__('-000') | item.__contains__('000a'))):
-                    print("found cover: " + os.path.basename(os.path.basename(item)) + " in " + file)
-                    cover_found = 1
-                    cbz_internal_covers_found += 1
-                    extract_cover(zip_file, file_path, image_file, root, file, full_path, os.path.splitext(file)[0],
-                                  item)
-                else:
-                    if ((item.endswith(".jpg") | item.endswith(".jpeg") | item.endswith(
-                            ".png") | item.endswith(".tbn ")) and cover_found != 1):
+                for string in cover_detection_strings:
+                    if item.__contains__(string):
+                        print("found cover: " + os.path.basename(os.path.basename(item)) + " in " + file)
                         cover_found = 1
-                        print("potential cover found: " + os.path.basename(item) + " in " + file_path)
-                        print("")
+                        cbz_internal_covers_found += 1
+                        extract_cover(zip_file, file_path, image_file, root, file, full_path, os.path.splitext(file)[0],
+                                      item)
                         break
+                    if item.__contains__("cover.xhtml"):
+                        print("cover.xhtml found in " + os.path.join(file_path, image_file))
+                        # Add logic to parse cover.xhtml for cover image name, then search and extract it.
+                    else:
+                        if ((item.endswith(".jpg") | item.endswith(".jpeg") | item.endswith(
+                                ".png") | item.endswith(".tbn ")) and cover_found != 1):
+                            cover_found = 1
+                            print("potential cover found: " + os.path.basename(item) + " in " + file_path)
+                            print("")
+                            break
 
     except zipfile.BadZipFile:
         print("Bad Zipfile.")
@@ -113,6 +128,7 @@ def individual_volume_cover_file_stuff(file, root, full_path):
     global file_count
     global cbz_count
     global epub_count
+    global cbr_count
 
     if file.endswith(".cbz") & os.path.isfile(os.path.join(root, file)):
         file_count += 1
@@ -125,6 +141,14 @@ def individual_volume_cover_file_stuff(file, root, full_path):
     if file.endswith(".epub") & os.path.isfile(os.path.join(root, file)):
         file_count += 1
         epub_count += 1
+        if not check_for_image(os.path.splitext(file)[0], root):
+            try:
+                check_internal_zip_for_cover(file, full_path, root)
+            except zlib.error:
+                print("Error -3 while decompressing data: invalid stored block lengths")
+    if file.endswith(".cbr") & os.path.isfile(os.path.join(root, file)):
+        file_count += 1
+        cbr_count += 1
         if not check_for_image(os.path.splitext(file)[0], root):
             try:
                 check_internal_zip_for_cover(file, full_path, root)
@@ -213,5 +237,6 @@ main()
 print("\nFor all " + str(len(paths)) + " paths.")
 print("Total Files Found: " + str(file_count))
 print("\t" + str(cbz_count) + " were cbz files")
+print("\t" + str(cbr_count) + " were cbr files")
 print("\t" + str(epub_count) + " were epub files")
 print("\tof those we found " + str(image_count) + " had a cover image file.")
